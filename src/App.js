b159fbead1c71735406b1e353abef4f3b127886b
Himanshu Garg <hgarg701@gmail.com>
2020-02-19 23:10:53 -0800

    Added first js file with React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+// import logo from './logo.svg';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>LIST OF UNOREDRED TASKS</h2>
+      <ul>
+        <li>This is TODO item One</li>
+        <li>This is TODO item Two</li>
+        <li>This is TODO item Three</li>
+      </ul>
     </div>
+
   );
 }
+
+ReactDOM.render(<App />, document.getElementById("root"))
 
 export default App;
